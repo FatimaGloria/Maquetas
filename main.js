9db0f45538d6adce5cd4ee7580a7afad7586de0c
Fatima J. Gloria <99113573+FatimaGloria@users.noreply.github.com>
2024-10-30 22:36:34 -0600

    2.1 Se agrega ultima parte del contenido de repaso.

--- main.js
+++ main.js
@@ -23,6 +23,74 @@ let concatenacion = nombre + " " + apellido;
 let datos = document.querySelector("#datos");
 // script
 datos.innerHTML = `
+    <hr/> 
     <h1> Esta es la caja de datos </h1>
     <h2> Mi nombre es: ${concatenacion} </h2>
 `;
+
+//Condiciones
+
+let altura = 187;
+
+if (altura >= 185){
+    datos.innerHTML += "<h1> Eres una persona alta </h1>";
+} else{
+    datos.innerHTML += "<h1> Eres una persona no muy alta </h1>"
+}
+
+//Bucles
+//  Estructura for
+for(inicializador; condicion; incrementador){
+    //condicion
+}
+
+for (let year = 2000; year <= 2023; year++) {
+    datos.innerHTML += `<h2>Estamos en el año: ${year} </h2>`;  
+}
+
+//Arrays "coleccion de variables"
+let nombres = ["Victor", "Francisco","Pepe"];
+let divNombres = document.querySelector("#nombres");
+
+divNombres.innerHTML = nombres[0];
+
+//  mostrar nombres en automatico.
+
+divNombres.innerHTML = "<h1> Lista de nombres </h1><ul>"
+
+//      funcion que va a recibir un valor (nombre)
+nombres.forEach(nombre => {
+    divNombres.innerHTML += "<li> +nombre+ </li>"
+});
+
+for(let nombre of nombres)
+    divNombres.innerHTML += "<li> +nombre+ </li>"
+});
+
+divNombres.innerHTML += "</ul>"
+
+//Funciones
+// variable = (paramétros) flecha llaves
+const miInformacion = (nombre, altura) => {
+//codigo que cada vez que se use la funcion se va a ejecutar.
+    let misDatos = `
+    <hr/> 
+    <h1> Esta es la caja de datos </h1>
+    <h2> Mi nombre es: ${nombre} </h2>
+    <h3> Mi estatura es: ${altura} </h3>    
+    `;
+
+    return misDatos; //Que regrese o imprima esa información.
+}
+
+//     invocar la función y agregarle los parametros.
+console.log(miInformacion("Victor", 187));
+
+// funcion que imprima a pantalla
+const Imprimir = () => {
+    let datos = document.querySelector("#datos");
+    //innerHTML sobre escribe la info a menos que se agregue +=
+    datos.innerHTML = miInformacion("Victor", 187)
+}
+
+Imprimir();
